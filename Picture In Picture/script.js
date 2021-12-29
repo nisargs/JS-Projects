b076dfe8f73c4dc5bbe53fcccd6130e40e86f817
nisargs <nisarg2198@gmail.com>
2021-12-29 11:03:06 +0530

    added functionality using JavaScript

--- Picture In Picture/script.js
+++ Picture In Picture/script.js
@@ -1,69 +1,28 @@
-const quoteContainer = document.getElementById('quote-container')
-const quoteText = document.getElementById('quote')
-const authorText = document.getElementById('author')
-const twitterBtn = document.getElementById('twitter')
-const newQuoteBtn = document.getElementById('new-quote')
-const loader = document.getElementById('loader');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
 
-let apiQuotes = [];
-
-// Show Loading
-function loading(){
-    loader.hidden = false;
-    quoteContainer.hidden = true;
-}
-
-// Hide Loading
-function complete(){
-    quoteContainer.hidden = false;
-    loader.hidden = true;
-}
-
-// Show New Quote
-function newQuote(){
-    loading();
-    // Pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    // Check if Author field is null and replace it with 'Unknown'
-    if(!quote.author){
-        authorText.textContent = 'Unknown';
-    } else {
-        authorText.textContent = quote.author;
-    }
-    
-    // Check Quote length to determine the styling
-    if (quote.text.length > 120){
-        quoteText.classList.add('long-quote');
-    } else {
-        quoteText.classList.remove('long-quote');
-    }
-    // Set Quote, Hide Loader
-    quoteText.textContent = quote.text;
-    complete();
-}
-
-// Get Quotes from API
-async function getQuotes(){
-    loading();
-    const apiURL = 'https://type.fit/api/quotes';
-    try{
-        const response = await fetch(apiURL);
-        apiQuotes = await response.json();
-        newQuote();
-    } catch (error){
-        // Handle Error here
+// Prompt the user to select a media stream, pass that to a video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        // Catch Error Here
+        console.log('Error occured: ', error);
     }
 }
 
-// To Tweet a Quote
-function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterUrl, '_blank');
-}
-
-// Event Listeners
-newQuoteBtn.addEventListener('click', newQuote);
-twitterBtn.addEventListener('click', tweetQuote);
+button.addEventListener('click', async () => {
+    // Disable the button
+    button.disabled = true;
+    // Start Picture-in-Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
 
 // On Load
-getQuotes();
+selectMediaStream();
