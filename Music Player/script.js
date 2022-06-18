7302511f9363b503dfebab102c981e9995a93734
nisargs <nisarg2198@gmail.com>
2022-06-18 09:47:13 +0530

    added play button logic for each song item

--- Music Player/script.js
+++ Music Player/script.js
@@ -27,14 +27,14 @@ songItems.forEach((element, i)=>{
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime <= 0){
         audioElement.play();
-        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-pause-circle');
         gif.style.opacity = 1;
     }
     else{
         audioElement.pause();
         masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');
+        masterPlay.classList.add('fa-circle-play');
         gif.style.opacity = 0;
     }
 })
@@ -48,4 +48,25 @@ audioElement.addEventListener('timeupdate', ()=>{
 
 myProgressBar.addEventListener('change', ()=>{
     audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = () =>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.add('fa-circle-play');
+        element.classList.remove('fa-pause-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        makeAllPlays();
+        index = parseInt(e.target.id);
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${index}.mp3`;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-pause-circle');
+    })
 })
